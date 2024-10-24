c380999f0ce16c71383015ec7d776c5cfce6153e
MWI Ost <MWI Ost@LAPTOP-F15KIDU8.anahuacmayab.inet>
2024-10-24 17:39:15 -0600

    failure test code in UserServiceTest - testUpdateUser

--- unittest/src/main/java/com/mayab/quality/loginunittest/dao/IDAOUser.java
+++ unittest/src/main/java/com/mayab/quality/loginunittest/dao/IDAOUser.java
@@ -1,9 +1,23 @@
 package com.mayab.quality.loginunittest.dao;
 
+import java.util.List;
+
 import com.mayab.quality.loginunittest.model.User;
 
 public interface IDAOUser {
+	User findByUserName(String name);
+
+	int save(User user);
+
+	User findUserByEmail(String email);
+
+	List<User> findAll();
+
+	User findById(int id);
+
+	boolean deleteById(int id);
+
+	User updateUser(User userOld);
 	
-	 User findByUserName(String name);
-	
+
 }
--- unittest/src/main/java/com/mayab/quality/loginunittest/model/User.java
+++ unittest/src/main/java/com/mayab/quality/loginunittest/model/User.java
@@ -1,9 +1,16 @@
 package com.mayab.quality.loginunittest.model;
 
 public class User {
+	private int id;
+	private String name;
 	private String email;
 	private String password;
 	private boolean isLogged;
+	
+		
+	public User(String name2, String email2, String password2) {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public String getEmail() {
 		return email;
@@ -22,6 +29,28 @@ public class User {
 	}
 	public void setLogged(boolean isLogged) {
 		this.isLogged = isLogged;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setToken(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getToken() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
