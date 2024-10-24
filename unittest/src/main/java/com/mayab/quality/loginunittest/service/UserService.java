74fd1febead1978f33341036dc6e908b1746e3f3
MWI Ost <MWI Ost@LAPTOP-F15KIDU8.anahuacmayab.inet>
2024-10-24 17:40:25 -0600

    failure testUpdateUser

---
+++ unittest/src/main/java/com/mayab/quality/loginunittest/service/UserService.java
@@ -0,0 +1,60 @@
+package com.mayab.quality.loginunittest.service;
+
+import java.util.*;
+
+import com.mayab.quality.loginunittest.dao.IDAOUser;
+import com.mayab.quality.loginunittest.model.User;
+
+public class UserService {
+	
+	private IDAOUser dao;
+	
+	public UserService(IDAOUser dao) {
+		this.dao = dao;
+	}
+	
+	public User createUser(String name, String email, String password) {
+		User user = null;
+		if (password.length() >= 8 && password.length() <=16) {
+			user = dao.findUserByEmail(email);
+			
+			if (user == null) {
+				user = new User(name,email,password);
+				int id = dao.save(user);
+				user.setId(id);
+			}
+		}
+		return user;
+	}
+	
+	public List<User> findAllUsers(){
+		List<User> users = new ArrayList<User>();
+		users = dao.findAll();
+	
+		return users;
+	}
+
+	public User findUserByEmail(String email) {
+		
+		return dao.findUserByEmail(email);
+	}
+
+	public User findUserById(int id) {
+		
+		return dao.findById(id);
+	}
+    
+    public User updateUser(User user) {
+    	User userOld = dao.findById(user.getId());
+    	System.out.println(user.getName());
+    	userOld.setName(user.getName());
+    	userOld.setPassword(user.getPassword());
+    	return dao.updateUser(user);
+    }
+
+    
+
+    boolean deleteUSer(int id) {
+    	return dao.deleteById(id);
+    }
+}
